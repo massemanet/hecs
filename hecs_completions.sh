7eef46d79f3a724636d698d3dca98d3193a8b302
mats cronqvist <masse@cronqvi.st>
2019-06-26 17:34:24 +0200

    refactored away task-defs and tasks

--- hecs_completions.sh
+++ hecs_completions.sh
@@ -7,10 +7,9 @@ _hecs_completions(){
         "1") COMPREPLY=($(compgen -W "$(hecs list-commands)" "${cur}"));;
         "2") COMPREPLY=($(compgen -W "$(hecs list-profiles)" "${cur}"));;
         "3") COMPREPLY=($(compgen -W "$(hecs list-clusters "${words[2]}")" "${cur}"));;
-        "4") COMPREPLY=($(compgen -W "$(hecs list-task-definitions "${words[2]}" "${words[3]}")" "${cur}"));;
-        "5") COMPREPLY=($(compgen -W "$(hecs list-tasks "${words[2]}" "${words[3]}" "${words[4]}")" "${cur}"));;
-        "6") COMPREPLY=($(compgen -W "$(hecs list-instances "${words[2]}" "${words[3]}" "" "${words[5]}")" "${cur}"));;
-        "7") COMPREPLY=($(compgen -W "$(hecs list-dockers "${words[2]}" "${words[3]}" "" "${words[5]}" "${words[6]}")" "${cur}"));;
+        "4") COMPREPLY=($(compgen -W "$(hecs list-services "${words[2]}" "${words[3]}")" "${cur}"));;
+        "5") COMPREPLY=($(compgen -W "$(hecs list-service-instances "${words[2]}" "${words[3]}" "${words[4]}")" "${cur}"));;
+        "6") COMPREPLY=($(compgen -W "$(hecs list-dockers "${words[2]}" "${words[3]}" "${words[4]}" "${words[5]}")" "${cur}"));;
         *) : ;;
     esac
     __ltrim_colon_completions "$cur"
